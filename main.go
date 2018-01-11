2c51c600755fc75a3ad8aa530bc5e8a8165b7ce1
shkelzeen <qashtu123>
2018-01-11 10:53:47 +0100

    tets

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"rest-api/service"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -17,4 +18,51 @@ func main() {
 	router.HandleFunc("/people/{id}", service.CreatePerson).Methods("POST")
 	router.HandleFunc("/people/{id}", service.DeletePerson).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+type AppContext struct {
+	Render  *render.Render
+	Version string
+	Env     string
+	Port    string
+	DB      DataStorer
+}
+
+// Healthcheck will store information about its name and version
+type Healthcheck struct {
+	AppName string `json:"appName"`
+	Version string `json:"version"`
+}
+
+// Status is a custom response object we pass around the system and send back to the customer
+// 404: Not found
+// 500: Internal Server Error
+type Status struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
+// CreateContextForTestSetup initialises an application context struct
+// for testing purposes
+func CreateContextForTestSetup() AppContext {
+	testVersion := "0.0.0"
+	db := CreateMockDatabase()
+	ctx := AppContext{
+		Render:  render.New(),
+		Version: testVersion,
+		Env:     local,
+		Port:    "3001",
+		DB:      db,
+	}
+	return ctx
+}
+
+// CreateMockDatabase initialises a database for test purposes
+func CreateMockDatabase() *MockDB {
+	list := make(map[int]User)
+	dt, _ := time.Parse(time.RFC3339, "1985-12-31T00:00:00Z")
+	list[0] = User{0, "John", "Doe", dt, "London"}
+	dt, _ = time.Parse(time.RFC3339, "1992-01-01T00:00:00Z")
+	list[1] = User{1, "Jane", "Doe", dt, "Milton Keynes"}
+	return &MockDB{list, 1}
 }
